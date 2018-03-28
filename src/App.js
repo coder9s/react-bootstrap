d4fb7543753ace7989ecd8760b01ecec83d5ffa0
Trần <33456536+coder9s@users.noreply.github.com>
2018-03-28 18:03:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import Product from './components/Product';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Product />
+          H
       </div>
     );
   }
